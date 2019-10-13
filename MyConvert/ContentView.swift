077ded9edf77dc3fcfb8c8f7f8a5a73ad9028ff7
Steven Brown <sgbrown@mac.com>
2019-10-13 14:07:22 +1300

    Initial Commit

---
+++ MyConvert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyConvert
+//
+//  Created by Steven Brown on 13/10/19.
+//  Copyright © 2019 Steven Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
